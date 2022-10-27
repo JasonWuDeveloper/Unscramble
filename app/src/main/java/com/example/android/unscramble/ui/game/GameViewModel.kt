e562d7c4326ce3b4ffd31fd8f01d3007d04cf266
Android developer <hbest@livemail.tw>
2022-10-27 17:59:00 +0800

    implement viewmodel function

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,6 +1,34 @@
 package com.example.android.unscramble.ui.game
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel:ViewModel() {
+
+    init {
+        Log.d("GameViewModel", "GameViewModel created!")
+        getNextWord()
+    }
+
+
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+    private var _count = 0
+    val count: Int
+        get() = _count
+    private var score = 0
+    private var currentWordCount = 0
+    private var _currentScrambledWord = "test"
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameViewModel","GameViewModel destroyed!")
+    }
 }
+
